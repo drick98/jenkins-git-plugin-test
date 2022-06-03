e2980896d1b4e95cdb75bd40c1069a9775de618f
drick98 <sdmpc98@gmail.com>
2022-06-03 11:28:06 +0600

    flask discarded

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route("/hello",methods=['GET'])
-def hello():
-    return "Hello Word, This is cinmoy"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002)
+print("Hi This is Cinmoy")
